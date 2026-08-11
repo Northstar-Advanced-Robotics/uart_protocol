5a7d8b87c93944e6ad4cab5a73287f8a50e2a702
david-erm <64047317+david-erm@users.noreply.github.com>
2026-08-11 15:46:54 -0600

    lib changes

--- uart_protocol.h
+++ uart_protocol.h
@@ -4,10 +4,10 @@
 #include <assert.h>
 #include <stdint.h>
 
+#define UART_HEAD_BYTE 0xA5
 #define UART_BAUD_RATE 115200
 
-// These are useful for declaring both C enums and CPP enums with single source of truth
-// Can also easily create Enum -> String with these
+// Single source of truth; I genereate callbacks + dispatch with these
 #define ARC_ROBOT_ID_LIST(X) \
     X(INVALID, 0)            \
     X(RED_HERO, 1)           \
@@ -29,23 +29,23 @@
     X(BLUE_DART, 108)        \
     X(BLUE_RADAR_STATION, 109)
 
-#define UART_MESSAGE_LIST(X)  \
-    X(TURRET_AIM_DATA, 1)     \
-    X(ROBOT_ID, 2)            \
-    X(ALIVE, 3)               \
-    X(ODOMETRY, 4)            \
-    X(AUTO_PATH, 5)           \
-    X(HEALTH, 6)              \
-    X(REF_TURRET_DATA, 7)     \
-    X(VISION_LOCALIZATION, 8) \
-    X(FLY_SKY_DATA, 9)        \
-    X(VT13_DATA, 10)          \
-    X(RESTART_DETECTOR, 11)
+#define UART_MESSAGE_LIST(X)                       \
+    X(TURRET_AIM_DATA, 1, uart_turret_aim_t)       \
+    X(ROBOT_ID, 2, uart_robot_id_t)                \
+    X(ALIVE, 3, void)                              \
+    X(ODOMETRY, 4, uart_odometry_t)                \
+    X(AUTO_PATH, 5, uart_autopath_t)               \
+    X(HEALTH, 6, uart_health_t)                    \
+    X(REF_TURRET_DATA, 7, void)                    \
+    X(VISION_LOCALIZATION, 8, uart_localization_t) \
+    X(FLY_SKY_DATA, 9, void)                       \
+    X(VT13_DATA, 10, void)                         \
+    X(RESTART_DETECTOR, 11, void)
 
 typedef uint16_t uart_message_t;
 enum
 {
-#define ENUM_MEMBER(name, val) UART_MESSAGE_##name = val,
+#define ENUM_MEMBER(name, val, ...) UART_MESSAGE_##name = (val),
     UART_MESSAGE_LIST(ENUM_MEMBER)
 #undef ENUM_MEMBER
 };
@@ -53,10 +53,19 @@ enum
 typedef uint16_t arc_robot_id_t;
 enum
 {
-#define ENUM_MEMBER(name, val) ARC_ROBOT_ID_##name = val,
+#define ENUM_MEMBER(name, val) ARC_ROBOT_ID_##name = (val),
     ARC_ROBOT_ID_LIST(ENUM_MEMBER)
 #undef ENUM_MEMBER
 };
+
+typedef struct __attribute__((packed))
+{
+    uint8_t head_byte;
+    uint16_t data_length;
+    uint8_t frame_sequence;
+    uint8_t crc8;
+    uart_message_t message_type;
+} uart_frame_header_t;
 
 typedef struct
 {
@@ -67,7 +76,7 @@ typedef struct
     uint8_t padding[2];
 } uart_turret_aim_t;
 static_assert(
-    sizeof(uart_turret_aim_t) == sizeof(float) * 3 + sizeof(arc_robot_id_t) + 2,
+    sizeof(uart_turret_aim_t) == (sizeof(float) * 3) + sizeof(arc_robot_id_t) + 2,
     "Size mismatch");
 
 typedef struct
@@ -126,6 +135,8 @@ typedef struct
     float heading;
     uint32_t timestamp;
 } uart_localization_t;
-static_assert(sizeof(uart_localization_t) == sizeof(float) * 3 + sizeof(uint32_t), "Size mismatch");
+static_assert(
+    sizeof(uart_localization_t) == (sizeof(float) * 3) + sizeof(uint32_t),
+    "Size mismatch");
 
 #endif  // #ifndef UART_PROTOCOL_H
